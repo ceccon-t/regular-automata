db8b22382b30b372811567207c83d1712d363e4e
Tiago Ceccon <ceccon.t@hotmail.com>
2018-10-21 17:44:55 -0200

    Adds NFA structure and a couple of tests

---
+++ src/main/java/ufrgs/inf/formais/automata/NFA.java
@@ -0,0 +1,76 @@
+package ufrgs.inf.formais.automata;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+import ufrgs.inf.formais.helper.State;
+import ufrgs.inf.formais.helper.StateSymbolTuple;
+import ufrgs.inf.formais.helper.Symbol;
+
+public class NFA extends Automaton {
+
+	protected String type = "Non-Deterministic Finite Automaton";
+	
+	private HashMap<StateSymbolTuple, HashSet<State>> transitionFunction;
+	
+	public NFA() {
+		
+	}
+	
+	public NFA(String name,
+				HashSet<Symbol> alphabet,
+				HashSet<State> states,
+				State initialState,
+				HashSet<State> finalStates,
+				HashMap<StateSymbolTuple, HashSet<State>> transitionFunction) {
+		this.name = name;
+		this.alphabet = alphabet;
+		this.states = states;
+		this.initialState = initialState;
+		this.finalStates = finalStates;
+		this.transitionFunction = transitionFunction;
+	}
+	
+	public String getType() {
+		return type;
+	}
+
+	public HashMap<StateSymbolTuple, HashSet<State>> getTransitionFunction() {
+		return transitionFunction;
+	}
+
+	public void setTransitionFunction(HashMap<StateSymbolTuple, HashSet<State>> transitionFunction) {
+		this.transitionFunction = transitionFunction;
+	}
+	
+	public boolean isDeterministic() {
+		for (Map.Entry<StateSymbolTuple, HashSet<State>> transition : transitionFunction.entrySet()) {
+			if (transition.getValue().size() > 1) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public String stringifyTransitionFunction() {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("\n- Transition function: \n");
+		for (Map.Entry<StateSymbolTuple, HashSet<State>> transition : transitionFunction.entrySet()) {
+			StateSymbolTuple key = transition.getKey();
+			State state = key.getState();
+			Symbol symbol = key.getSymbol();
+			for (State destiny : transition.getValue()) {
+				sb.append("( " + state + " , " + symbol + " ) => " + destiny + "\n");
+			}
+		}
+		sb.append("\n-End of transition function-\n");
+		
+		return sb.toString();
+	}
+	
+	
+
+}
