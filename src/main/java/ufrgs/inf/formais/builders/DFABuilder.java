469bdf4a697b2693102c5640c7aaca2672252a4c
Tiago Ceccon <ceccon.t@hotmail.com>
2018-10-24 23:25:22 -0200

    Refactors dfa builder shared properties with nfa into abstract automaton builder

--- src/main/java/ufrgs/inf/formais/builders/DFABuilder.java
+++ src/main/java/ufrgs/inf/formais/builders/DFABuilder.java
@@ -9,17 +9,7 @@ import ufrgs.inf.formais.helper.State;
 import ufrgs.inf.formais.helper.StateSymbolTuple;
 import ufrgs.inf.formais.helper.Symbol;
 
-public class DFABuilder {
-	
-	private String name;
-	
-	private HashSet<Symbol> alphabet;
-	
-	private HashSet<State> states;
-	
-	private State initialState;
-	
-	private HashSet<State> finalStates;
+public class DFABuilder extends AutomatonBuilder {
 	
 	private HashMap<StateSymbolTuple, State> transitionFunction;
 	
@@ -28,112 +18,6 @@ public class DFABuilder {
 		this.states = new HashSet<State>();
 		this.finalStates = new HashSet<State>();
 		this.transitionFunction = new HashMap<StateSymbolTuple, State>();
-	}
-	
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public void addSymbolToAlphabetFromString(String symbol) {
-		this.alphabet.add(new Symbol(symbol));
-	}
-	
-	public void addSymbolToAlphabet(Symbol symbol) {
-		this.alphabet.add(symbol);
-	}
-	
-	public void addSymbolListToAlphabetFromString(List<String> symbols) {
-		for (String symbol : symbols) {
-			this.alphabet.add(new Symbol(symbol));
-		}
-	}
-	
-	public void addSymbolListToAlphabet(List<Symbol> symbols) {
-		for (Symbol symbol : symbols) {
-			this.alphabet.add(symbol);
-		}
-	}
-	
-	public void addStateFromString(String state) {
-		this.states.add(new State(state));
-	}
-	
-	public void addState(State state) {
-		this.states.add(state);
-	}
-	
-	public void addStateListFromString(List<String> states) {
-		for (String state : states) {
-			this.states.add(new State(state));
-		}
-	}
-	
-	public void addStateList(List<State> states) {
-		for (State state : states) {
-			this.states.add(state);
-		}
-	}
-	
-	public void setInitialStateFromString(String initialStateName) {
-		State initialState = new State(initialStateName);
-		if (!this.states.contains(initialState)) {
-			// TODO: implement exceptions 
-			//throw new Exception("Tried to add a non-existing state as initial state during DFA creation");
-		} else {
-			this.initialState = initialState;
-		}
-	}
-	
-	public void setInitialState(State initialState) {
-		if (!this.states.contains(initialState)) {
-			// TODO: implement exceptions 
-			//throw new Exception("Tried to add a non-existing state as initial state during DFA creation");
-		} else {
-			this.initialState = initialState;
-		}
-	}
-	
-	public void addFinalStateFromString(String finalStateString) {
-		State finalState = new State(finalStateString);
-		if (!this.states.contains(finalState)) {
-			// TODO: implement exceptions 
-			//throw new Exception("Tried to add a non-existing state as initial state during DFA creation");
-		} else {
-			this.finalStates.add(finalState);
-		}
-	}
-	
-	public void addFinalState(State finalState) {
-		if (!this.states.contains(finalState)) {
-			// TODO: implement exceptions 
-			//throw new Exception("Tried to add a non-existing state as initial state during DFA creation");
-		} else {
-			this.finalStates.add(finalState);
-		}
-	}
-	
-	public void addFinalStateListFromString(List<String> finalStateStrings) {
-		for (String finalStateString : finalStateStrings) {
-			State finalState = new State(finalStateString);
-			if (!this.states.contains(finalState)) {
-				// TODO: implement exceptions 
-				//throw new Exception("Tried to add a non-existing state as initial state during DFA creation");
-			} else {
-				this.finalStates.add(finalState);
-			}
-		}
-	}
-	
-	public void addFinalStateList(List<State> finalStates) {
-		for (State finalState : finalStates) {
-			if (!this.states.contains(finalState)) {
-				// TODO: implement exceptions 
-				//throw new Exception("Tried to add a non-existing state as initial state during DFA creation");
-			} else {
-				this.finalStates.add(finalState);
-			}
-		}
 	}
 	
 	public void addTransitionFromStrings(String stateString, String symbolString, String destinyStateString) {
