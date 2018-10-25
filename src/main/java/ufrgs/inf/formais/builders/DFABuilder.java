809016e06673cdbbeedc964a3665c9854ca913e2
Tiago Ceccon <ceccon.t@hotmail.com>
2018-10-24 23:51:33 -0200

    Adds builder for nfa and a couple of tests for it

--- src/main/java/ufrgs/inf/formais/builders/DFABuilder.java
+++ src/main/java/ufrgs/inf/formais/builders/DFABuilder.java
@@ -2,7 +2,6 @@ package ufrgs.inf.formais.builders;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 
 import ufrgs.inf.formais.automata.DFA;
 import ufrgs.inf.formais.helper.State;
@@ -24,11 +23,11 @@ public class DFABuilder extends AutomatonBuilder {
 		State state = new State(stateString);
 		Symbol symbol = new Symbol(symbolString);
 		State destinyState = new State(destinyStateString);
-		if (!this.alphabet.contains(symbol) || !this.states.contains(state) || !this.states.contains(destinyState)) {
+		StateSymbolTuple stateSymbol = new StateSymbolTuple(state, symbol);
+		if (!this.alphabet.contains(symbol) || !this.states.contains(state) 
+				|| !this.states.contains(destinyState) || this.transitionFunction.containsKey(stateSymbol)) {
 			// TODO: implement exceptions 
-			//throw new Exception("Tried to add a non-existing state as initial state during DFA creation");
 		} else {
-			StateSymbolTuple stateSymbol = new StateSymbolTuple(state, symbol);
 			this.transitionFunction.put(stateSymbol, destinyState);
 		}
 	}
@@ -38,7 +37,6 @@ public class DFABuilder extends AutomatonBuilder {
 		if (!this.alphabet.contains(symbol) || !this.states.contains(state) 
 				|| !this.states.contains(destinyState) || this.transitionFunction.containsKey(stateSymbol)) {
 			// TODO: implement exceptions 
-			//throw new Exception("Tried to add a non-existing state as initial state during DFA creation");
 		} else {
 			this.transitionFunction.put(stateSymbol, destinyState);
 		}
